075d49e4249a0ee23b624dfa0caf78991e6faaea
Khanh Le <masterbruculous@gmail.com>
2024-09-16 22:30:28 +0700

    Initial Commit

---
+++ NewVocab/NewVocabApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewVocabApp.swift
+//  NewVocab
+//
+//  Created by MasterBi on 16/9/24.
+//
+
+import SwiftUI
+
+@main
+struct NewVocabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
